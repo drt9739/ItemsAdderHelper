d93394e665f4c3dfc36f73cc2ced543be2a836aa
drt9739 <grigchita@gmail.com>
2023-10-11 00:01:09 +0900

    added support for 7tv api

---
+++ Scripts/7tv.py
@@ -0,0 +1,76 @@
+import requests
+import json
+
+
+class SevenTvEmote:
+    url = 'https://cdn.7tv.app/emote/'
+
+    def __init__(self, name: str, author: str, animated: bool, resolution: list, emote_id: str):
+        self.name = name
+        self.author = author
+        self.animated = animated
+        self.resolution = resolution
+        self.emote_id = emote_id
+
+        self.better_res = max(resolution, key=lambda x: x['width'] and x['height'])
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __repr__(self):
+        return f"{self.name}"
+
+    def if_animated(self) -> bool:
+        return self.animated
+
+    def emote_name(self) -> str:
+        return self.name
+
+    def get_better_res(self):
+        return self.better_res
+
+
+class SevenTvApi:
+    api = 'https://7tv.io/v3/'
+
+    def __init__(self):
+        pass
+
+    def get_emote(self, url: str) -> object:
+        """
+        Получает ссылку и обращается по id эмоута к 7tv api
+        После создаёт объект класса `SevenTvEmote()` и возвращает его пользователю
+        :param url: str
+        :return: SevenTvEmote()
+        """
+        emote_id = url[16:]
+        result = requests.get(self.api + emote_id).json()
+
+        author = result["owner"]["display_name"]
+        name = result["name"]
+        animated = result["animated"]
+        resolution = result["host"]["files"]
+
+        return SevenTvEmote(name, author, animated, resolution, emote_id)
+
+    def get_emote_set(self, url: str) -> list:
+        """
+        Получает ссылку и обращается к сету эмотов по id
+        Возращает список создержащий объекты эмоутов `SevenTvEmotes()`
+        :param url: str
+        :return: [SevenTvEmotes()...]
+        """
+        set_id = url[16:]
+        result = requests.get(self.api + set_id).json()
+
+        emote_list = list()
+
+        for emote in result["emotes"]:
+            author = emote["data"]["owner"]["display_name"]
+            name = emote["name"]
+            animated = emote["data"]["animated"]
+            resolution = emote["data"]["host"]["files"]
+            emote_id = emote["data"]["id"]
+            emote_list.append(SevenTvEmote(name, author, animated, resolution, emote_id))
+
+        return emote_list
